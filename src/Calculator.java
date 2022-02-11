6dfcb0f136eb52f34ada1d8043f2cee1f5cbf8b8
juancamiloarrieta <juancamilo.arrieta@globant.com>
2022-02-11 18:30:31 -0500

    "Adding methods sum and rest to Calculator class"

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,6 +2,14 @@ package src;
 
 public class Calculator {
 
+    public int sum(int number1, int number2){
+        return number1+number2;
+    }
+
+    public int rest(int number1, int number2){
+        return number1-number2;
+    }
+
     public static void main(String[] args) {
 
     }
