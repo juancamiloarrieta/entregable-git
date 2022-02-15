fad3463f2c7b55e1360c70c09bd0b4cf1f66c7a4
juancamiloarrieta <99158195+juancamiloarrieta@users.noreply.github.com>
2022-02-14 19:09:01 -0500

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,6 +2,8 @@ package src;
 
 public class Calculator{
 
+    public static void main(String[] args) {
+        
     public int divide(int number1, int number2){
         return number1/number2;
     }
@@ -17,8 +19,6 @@ public class Calculator{
     public int rest(int number1, int number2){
         return number1-number2;
     }
-
-    public static void main(String[] args) {
 
     }
 
